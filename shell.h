97e2790637d7d90a42a72d69aca74a87f44a0a22
andres-f-ariza <arizaandres8@gmail.com>
2023-04-17 13:54:20 -0700

    fix:main.Modifications towards Betty coding style

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define _GNU_SOURCE
+
+/* libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+
+#endif
