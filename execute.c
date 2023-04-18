d02e7c383cac72be4e01857fdafeb34040ce7fbf
andres-f-ariza <arizaandres8@gmail.com>
2023-04-18 06:28:38 -0700

    fix.execute.2

--- execute.c
+++ execute.c
@@ -1,32 +1,39 @@
-#include "shell.h"
 /**
- *execute - function that executes a given command
- *@args: array of arguments passed with the command
-*Return: void
-*/
+ * execute - function that executes a given command
+ * @args: array of arguments passed with the command
+ * Return: void
+ */
 void execute(char **args)
 {
 	pid_t pid;
 	int status;
 
-        pid = fork();
+	pid = fork();
 	if (pid == 0)
 	{
-if (execve(args[0], args, environ) == -1)
+		/* Child process */
+		if (args && args[0])
 		{
+			execve(args[0], args, NULL);
 			perror("execute");
+			exit(EXIT_FAILURE);
 		}
-		exit(EXIT_FAILURE);
+		else
+		{
+			fprintf(stderr, "execute: No command specified\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else if (pid < 0)
 	{
+		/* Error forking */
 		perror("fork");
 	}
 	else
 	{
-		         do
-			 {
-				 wait(&status);
-}while(!WIFEXITED(status) && !WIFSIGNALED(status));
-}
+		/* Parent process */
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
