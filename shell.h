e45a642c3dae9f393814532ecbf08ed6b37fab29
andres-f-ariza <arizaandres8@gmail.com>
2023-04-20 09:50:43 -0700

    fix:parse.error: conflicting types for ‘find_executable’

--- shell.h
+++ shell.h
@@ -19,7 +19,7 @@
 
 char **parse_line(char *line);
 void execute(char *executable_path, char **args);
-char *find_executable(char *command);
+char *find_executable(char* executable_name, char* path);
 char *prompt(void);
 char *find_path(char *command);
 
